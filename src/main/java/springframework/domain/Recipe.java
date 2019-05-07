c9bbba990e24887c442a9ea6e483264ce580fa59
siddharthashankarblore <sonumca04@gmail.com>
2019-05-07 14:22:54 +0530

    many to many relationship added between category and recipe.

--- src/main/java/springframework/domain/Recipe.java
+++ src/main/java/springframework/domain/Recipe.java
@@ -1,8 +1,5 @@
 package springframework.domain;
 
-import org.hibernate.mapping.Value;
-
-import javax.annotation.Generated;
 import javax.persistence.*;
 import java.util.Set;
 
@@ -32,6 +29,12 @@ public class Recipe {
 
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+    joinColumns =@JoinColumn(name = "recipe_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
