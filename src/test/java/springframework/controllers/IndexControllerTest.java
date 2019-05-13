d2942829d0f566410facdaa541a4553930456433
siddharthashankarblore <sonumca04@gmail.com>
2019-05-13 17:29:08 +0530

    argument captur used in IndexController Test class.

--- src/test/java/springframework/controllers/IndexControllerTest.java
+++ src/test/java/springframework/controllers/IndexControllerTest.java
@@ -2,10 +2,15 @@ package springframework.controllers;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+import springframework.domain.Recipe;
 import springframework.services.RecipeService;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -28,10 +33,26 @@ public class IndexControllerTest {
     @Test
     public void getIndexPage() {
 
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         String viewMessage = indexController.getIndexPage(model);
+
+        //then
         assertEquals("index", viewMessage);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
 
     }
 }
