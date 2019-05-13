ea7224c7f23e958cc7673de191ee6e86aba732a0
siddharthashankarblore <sonumca04@gmail.com>
2019-05-13 16:38:22 +0530

    Used Mockito Mock to test the business logic inside service layer.

---
+++ src/test/java/springframework/services/RecipeServiceImplTest.java
@@ -0,0 +1,40 @@
+package springframework.services;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import springframework.domain.Recipe;
+import springframework.repositories.RecipeRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+
+        Recipe recipe = new Recipe();
+        HashSet recipeData = new HashSet<>();
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 0);
+        verify(recipeRepository, times(3)).findAll();
+    }
+}
