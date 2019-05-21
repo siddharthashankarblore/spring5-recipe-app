dc7de876665f2db79ac2b36059d452969a9d2004
siddharthashankarblore <sonumca04@gmail.com>
2019-05-21 17:03:11 +0530

    new recipe creation form and controller changes.

--- src/main/java/springframework/controllers/RecipeController.java
+++ src/main/java/springframework/controllers/RecipeController.java
@@ -2,8 +2,11 @@ package springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springframework.commands.RecipeCommand;
 import springframework.services.RecipeService;
 
 @Controller
@@ -19,5 +22,18 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
