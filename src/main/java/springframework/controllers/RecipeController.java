a8618a7029c326d038119407958851cb9e455246
siddharthashankarblore <sonumca04@gmail.com>
2019-06-10 15:56:43 +0530

    404 error page added to display in case of 404 page not found.

--- src/main/java/springframework/controllers/RecipeController.java
+++ src/main/java/springframework/controllers/RecipeController.java
@@ -1,11 +1,18 @@
 package springframework.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
 import springframework.commands.RecipeCommand;
+import springframework.exceptions.NotFoundException;
 import springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -48,5 +55,18 @@ public class RecipeController {
     public String deleteById(@PathVariable String id){
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
